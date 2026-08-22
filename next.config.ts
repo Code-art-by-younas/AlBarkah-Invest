45a08850dbd20463331e83613b7843a4531b6f8a
Muhammad younas <my9686934@gmail.com>
2026-08-22 13:33:25 +0500

    Enhance Next.js config with optimizations and patterns

--- next.config.ts
+++ next.config.ts
@@ -1,5 +1,19 @@
 import type { NextConfig } from "next";
 
-const nextConfig: NextConfig = {};
+const nextConfig: NextConfig = {
+  swcMinify: true,
+  images: {
+    remotePatterns: [
+      {
+        protocol: "https",
+        hostname: "res.cloudinary.com",
+      },
+    ],
+  },
+  serverExternalPackages: ["drizzle-orm", "pg", "bcryptjs", "jose"],
+  experimental: {
+    optimizePackageImports: ["react-icons", "date-fns", "lucide-react"],
+  },
+};
 
 export default nextConfig;
