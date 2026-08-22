5960d5749ee4103525944435c4eb1e832fc6b784
Muhammad younas <my9686934@gmail.com>
2026-08-22 13:38:17 +0500

    Add remote pattern for Cloudinary images

--- next.config.ts
+++ next.config.ts
@@ -1,12 +1,15 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  swcMinify: true,
   images: {
     remotePatterns: [
       {
         protocol: "https",
         hostname: "res.cloudinary.com",
+      },
+      {
+        protocol: "https",
+        hostname: "*.cloudinary.com",
       },
     ],
   },
